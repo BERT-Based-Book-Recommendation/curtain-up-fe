f33522a2fbb143908bbaa4bab5e934c98c15f4d0
Solmi <7548ck@naver.com>
2024-06-12 15:36:44 +0900

    feat: header 구현

---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header: React.FC = () => {
+  return (
+    <HeaderContainer>
+      <WrapLogo>
+        <img src="/assets/logo.svg" alt="로고" />
+        <h1>리북</h1>
+      </WrapLogo>
+      <WrapMenus>
+        <div>서비스 소개</div>
+        <div>도서 추천</div>
+        <div>도서 추천 리스트</div>
+        <div>서비스 문의</div>
+      </WrapMenus>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 25px;
+`;
+const WrapLogo = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    width: 70px;
+  }
+  h1 {
+    padding: 10px 0 0 1rem;
+  }
+`;
+
+const WrapMenus = styled.div`
+  display: flex;
+  gap: 20px;
+  padding-top: 10px;
+  flex-wrap: wrap;
+`;
