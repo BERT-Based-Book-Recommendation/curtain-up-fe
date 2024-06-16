413cb800d1d304d8bbf3d0616e183c0013c89045
Solmi <7548ck@naver.com>
2024-06-16 22:09:07 +0900

    feat: 추천 기준 내용 삽입

--- src/pages/IntroductionIndexPage.tsx
+++ src/pages/IntroductionIndexPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import bookList from "../constants/bookList";
 import Marquee from "react-fast-marquee";
+import { motion } from "framer-motion";
 
 const IntroductionIndexPage = () => {
   return (
@@ -9,7 +10,7 @@ const IntroductionIndexPage = () => {
       <HomeContainer>
         <WrapText>
           <p>
-            내가 읽고 싶은 도서를 <br /> 내용 기반으로 추천해주는 서비스{" "}
+            내가 읽고 싶은 도서를 <br /> 내용 기반으로 추천해주는 AI 서비스{" "}
           </p>
           <button>도서 추천 받으러 가기</button>
         </WrapText>
@@ -20,8 +21,8 @@ const IntroductionIndexPage = () => {
       </HomeContainer>
       <BookListContainer>
         <h1>
-          총 0000권의 도서 데이터 <br /> 000000개의 리뷰 데이터를 활용한 도서
-          추천 AI 서비스
+          총 1,325권의 도서 데이터 <br /> 13,250개의 리뷰 데이터를 활용한 도서
+          추천 서비스
         </h1>
         <WrapCards>
           <Marquee speed={80} pauseOnHover={true}>
@@ -40,36 +41,56 @@ const IntroductionIndexPage = () => {
         <img src="/assets/wave-object.png" alt="backgroud" />
         <h1>리북만의 도서 추천 기준</h1>
         <WrapSection>
-          <Section>
-            <div className="icon" />
-            <h3> 첫 번째 기준 </h3>
-            <p>
-              {" "}
-              첫 번째 기준입니다. 첫 번째 기준입니다. 첫 번째 기준입니다. 첫
-              번째 기준입니다. 첫 번째 기준입니다. 첫 번째 기준입니다. 첫 번째
-              기준입니다.{" "}
-            </p>
-          </Section>
-          <Section>
-            <div className="icon" />
-            <h3> 두 번째 기준 </h3>
-            <p>
-              {" "}
-              두 번째 기준입니다. 두 번째 기준입니다. 두 번째 기준입니다. 두
-              번째 기준입니다. 두 번째 기준입니다. 두 번째 기준입니다. 두 번째
-              기준입니다.{" "}
-            </p>
-          </Section>
-          <Section>
-            <div className="icon" />
-            <h3> 세 번째 기준 </h3>
-            <p>
-              {" "}
-              세 번째 기준입니다. 세 번째 기준입니다. 세 번째 기준입니다. 세
-              번째 기준입니다. 세 번째 기준입니다. 세 번째 기준입니다. 세 번째
-              기준입니다.{" "}
-            </p>
-          </Section>
+          <motion.a
+            className="section"
+            whileHover={{ scale: 1.2 }}
+            onHoverStart={(e) => {}}
+            onHoverEnd={(e) => {}}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+          >
+            <Section>
+              <img src="/assets/1.png" alt="icon" className="icon" />
+              <h3> 내용 기반 추천 </h3>
+              <p>
+                영화나 드라마에만 예고편이 필요한 것이 아닙니다. 이제 여러분이
+                읽고 싶은 내용의 도서를 AI로 추천받으세요!
+              </p>
+            </Section>
+          </motion.a>
+
+          <motion.a
+            className="section"
+            whileHover={{ scale: 1.2 }}
+            onHoverStart={(e) => {}}
+            onHoverEnd={(e) => {}}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+          >
+            <Section>
+              <img src="/assets/2.jpg" alt="icon" className="icon" />
+              <h3> 선호도 기반 추천 알고리즘 </h3>
+              <p>
+                교보문고에서 제공하는 '함께 구매한 도서 목록' 데이터를 사용하여
+                다른 사용자들의 선호도를 기반으로 한 추천 알고리즘을 제공합니다.
+              </p>
+            </Section>
+          </motion.a>
+
+          <motion.a
+            className="section"
+            whileHover={{ scale: 1.2 }}
+            onHoverStart={(e) => {}}
+            onHoverEnd={(e) => {}}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+          >
+            <Section>
+              <img src="/assets/3.png" alt="icon" className="icon" />
+              <h3> 생성형 AI 책 소개 </h3>
+              <p>
+                도서 추천 모델이 제공하는 책을 생성형 AI를 활용하여 흥미로운
+                문구와 함께 추천해드립니다!
+              </p>
+            </Section>
+          </motion.a>
         </WrapSection>
       </RecommendStandard>
     </Introduction>
@@ -135,6 +156,7 @@ const WrapCards = styled.div`
   gap: 20px;
 `;
 const CardImg = styled.img`
+  width: 220px;
   height: 320px;
   object-fit: cover;
   border-radius: 30px;
@@ -149,7 +171,7 @@ const RecommendStandard = styled.div`
   align-items: center;
 
   h1 {
-    margin: 30px 0;
+    margin: 60px 0;
     letter-spacing: 11px;
   }
 
@@ -162,11 +184,8 @@ const WrapSection = styled.div`
   justify-content: space-evenly;
   text-align: center;
 
-  .icon {
-    width: 200px;
-    height: 200px;
-    border-radius: 30px;
-    background-color: lightgray;
+  .section {
+    width: 30%;
   }
 `;
 
@@ -175,10 +194,19 @@ const Section = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  max-width: 25%;
+  background-color: #fffbf6;
+
+  .icon {
+    width: 200px;
+    height: 200px;
+    border-radius: 30px;
+    background-color: #fffbf6;
+    object-fit: cover;
+  }
 
   p {
     margin: 0;
+    width: 70%;
   }
 `;
 
